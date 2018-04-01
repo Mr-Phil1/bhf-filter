358406eb9479480c594b27402f5d5674ce548c5f
Mr Phil <mr.phil1@gmx.net>
2018-04-01 11:51:58 +0200

    no message

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,7 @@
-function popup(URL) {
+function popupxl(URL) {
+w = window.open(URL, "", "toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=1366,height=768,left = 480,top = 234");
+}
+
+function popupmin(URL) {
 w = window.open(URL, "", "toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=400,height=260,left = 480,top = 234");
 }
