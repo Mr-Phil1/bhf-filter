490dc469c6c187fd7ec4a6ab95d26eef7b603f0c
Mr. Phil1 <mr.phil1@gmx.net>
2018-08-28 13:53:42 +0200

    no message

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,10 @@
-function popupxl(URL) {
-w = window.open(URL, "", "toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=1366,height=768,left = 480,top = 234");
+function popup4k(URL) {
+w = window.open(URL, "", "toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=3840,height=2160,left = 480,top = 234");
+}
+
+function popup(URL) {
+  w = window.open(URL,"", "toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=1366,height=768,")
+
 }
 
 function popupmin(URL) {
